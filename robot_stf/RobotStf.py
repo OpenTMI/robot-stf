634d40b105cdceac411f7117fd411b3c6c111a2b
Jussi Vatjus-Anttila <jussiva@gmail.com>
2021-03-22 08:11:33 +0200

    wip

--- robot_stf/RobotStf.py
+++ robot_stf/RobotStf.py
@@ -7,59 +7,76 @@ import atexit
 
 
 class RobotStf:
-    """ RobotFramework Lockable plugin """
+    """ RobotFramework STF plugin """
     ROBOT_LIBRARY_SCOPE = 'GLOBAL'
     ROBOT_LIBRARY_VERSION = __version__
 
     def __init__(self, hostname, token):
         self._stf = StfClient(hostname)
         self._stf.connect(token)
-        self._adb = AdbServer()
-        self._appium = Appium()
-        self._device = None
-
-    def lock(self, requirements: dict, timeout_seconds: int = 60*60):
-        """ Allocate phone and return it's details """
-        self._device = self._stf.find_and_allocate(requirements=requirements, timeout_seconds=timeout_seconds)
+        self._devices = list()
 
         @atexit.register
         def exit():
             nonlocal self
             self.exit()
 
-        return self._device
+    def lock(self, requirements: dict, timeout_seconds: int = 60*60):
+        """ Allocate phone and return it's details """
+        device = self._stf.find_and_allocate(requirements=requirements, timeout_seconds=timeout_seconds)
+        self._devices.append(device)
+        return device
 
     def exit(self):
-        if not self._device:
-            return
-        if self._device.get('owner') is not None:
-            self.logger.warn(f"exit:Release device {self._device.get('serial')}")
-            self.unlock()
-            self.stop_appium()
-            self.stop_adb()
+        for device in self._devices:
+            if device.get('owner') is not None:
+                self.logger.warn(f"exit:Release device {device.get('serial')}")
+                self.unlock(device)
+                self.stop_appium(device)
+                self.stop_adb(device)
 
-    def unlock(self):
-        assert self._device, 'device not locked'
-        self._stf.release(self._device)
+    def unlock(self, device):
+        assert device.get('owner'), 'device not locked'
+        self._stf.release(device)
 
-    def start_adb(self):
-        assert self._device, 'device not locked'
-        adb_adr = self._stf.remote_connect(self._device)
-        self._device['remote_adb_url'] = adb_adr
-        self._adb.connect(adb_adr)
+    def setup_appium(self, device):
+        self.start_adb(device)
+        self.start_appium(device)
 
-    def stop_adb(self):
-        assert self._device, 'device not locked'
-        self._adb.stop()
-        self._stf.remote_disconnect(self._device)
-        self._device['remote_adb_url'] = None
+    def teardown_appium(self, device):
+        self.stop_adb(device)
+        self.stop_appium(device)
 
-    def start_appium(self):
-        assert self._device, 'device not locked'
+    def start_adb(self, device):
+        assert device, 'device not locked'
+        assert not device.get('adb'), 'device adb is already running'
+        adb_adr = self._stf.remote_connect(device)
+        device['remote_adb_url'] = adb_adr
+        adb = AdbServer()
+        device['adb'] = adb
+        adb.connect(adb_adr)
+
+    def stop_adb(self, device):
+        assert device, 'device not locked'
+        adb = device.get('adb')
+        assert adb, 'device adb not running'
+        adb.stop()
+        self._stf.remote_disconnect(device)
+        device['remote_adb_url'] = None
+        device['adb'] = None
+
+    def start_appium(self, device):
+        assert device, 'device not locked'
+        assert not device.get('appium'), 'device appium is already running'
+        appium = Appium()
+        device['appium'] = appium
         """ Start appium in local host and return appium url """
-        self._appium.start()
-        return f'http://localhost:{self._appium.port}'
+        appium.start()
+        return f'http://localhost:{appium.port}'
 
-    def stop_appium(self):
-        assert self._device, 'device not locked'
-        self._appium.stop()
+    def stop_appium(self, device):
+        assert device, 'device not locked'
+        assert device.get('appium'), 'device appium is not running'
+        appium = device.get('appium')
+        appium.stop()
+        device['appium'] = None
